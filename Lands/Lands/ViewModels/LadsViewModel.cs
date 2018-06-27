50cada8cf610e86fc0863fafe0da5f2ac1820880
juanbuiles67227 <31153055+juanbuiles67227@users.noreply.github.com>
2018-06-27 06:21:03 -0500

    ListView LandsPage

--- Lands/Lands/ViewModels/LadsViewModel.cs
+++ Lands/Lands/ViewModels/LadsViewModel.cs
@@ -1,10 +1,12 @@
 ﻿namespace Lands.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
     using Models;
     using Services;
     using System;
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
+    using System.Windows.Input;
     using Xamarin.Forms;
 
     public class LadsViewModel : BaseViewModel
@@ -15,6 +17,7 @@
 
         #region Attributes
         private ObservableCollection<Land> lands;
+        private bool isRefreshing;
         #endregion
 
         #region Properties
@@ -23,6 +26,13 @@
             get { return this.lands; }
             set { SetValue(ref this.lands, value); }
         }
+
+        public bool IsRefreshing
+        {
+            get { return this.isRefreshing; }
+            set { SetValue(ref this.isRefreshing, value); }
+        }
+
         #endregion
 
         #region Contructors
@@ -36,23 +46,51 @@
         #region Methods
         private async void LoadLands()
         {
+            this.IsRefreshing = true;
+            var connection = await this.apiService.CheckConnection();
+
+            if (!connection.IsSuccess)
+            {
+                this.IsRefreshing = false;
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    connection.Message,
+                    "Accept");
+                await Application.Current.MainPage.Navigation.PopAsync();
+                return;
+            }
+
             var response = await this.apiService.GetList<Land>(
                 "http://restcountries.eu",
                 "/rest",
                 "/v2/all");
 
-            if (response.IsSuccess)
+            if (!response.IsSuccess)
             {
+                this.IsRefreshing = false;
                 await Application.Current.MainPage.DisplayAlert(
                     "Error",
                     response.Message,
                     "Accept");
+                await Application.Current.MainPage.Navigation.PopAsync();
                 return;
             }
 
             var list = (List<Land>)response.Result;
             this.Lands = new ObservableCollection<Land>(list);
-        } 
+            this.IsRefreshing = false;
+        }
+        #endregion
+
+        #region Commands
+        public ICommand RefreshCommand
+        {
+            get
+            {
+                return new RelayCommand(LoadLands);
+            }
+        }
+
         #endregion
     }
 }
